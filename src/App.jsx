6d1cfdabccad9295c81f5aeb5f22f24c3c67b61f
Furkan Demirel <furkandemireleng@gmail.com>
2024-04-30 01:55:39 +0300

    Create resuable button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Button from "./components/Button.jsx";
 
-function App() {
-  return (
-    <>
-        <div className="w-20 h-20 bg-amber-700">
-            <p className="text-2xl">text nana</p>
-        </div>
-    </>
-  )
+const App = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Hello World</h1>
+            <div className="pt-[3.75rem] lg:pt-[6.75rem] ">
+                <Button className="mt-2.5" href="#link">
+                    Click me
+                </Button>
+            </div>
+            <ButtonGradient/>
+
+        </>
+    )
 }
 
 export default App
