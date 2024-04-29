f23b08646ddb14b0af2f545496e9c923a22cf319
Furkan Demirel <furkandemireleng@gmail.com>
2024-04-30 00:16:51 +0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+        <div className="w-20 h-20 bg-amber-700">
+            <p className="text-2xl">text nana</p>
+        </div>
+    </>
+  )
+}
+
+export default App
