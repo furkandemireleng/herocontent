bfda2c7e99d1dd645ff1eb9923c6d6e149b0f25b
Furkan Demirel <furkandemireleng@gmail.com>
2024-05-01 01:26:52 +0300

    complete header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
-import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
 
 const App = () => {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello World</h1>
-            <div className="pt-[3.75rem] lg:pt-[6.75rem] ">
-                <Button className="mt-2.5" href="#link">
-                    Click me
-                </Button>
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                <Header />
             </div>
             <ButtonGradient/>
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import ButtonSvg from "../assets/svg/ButtonSvg.jsx";
 const Button = ({className, href, onClick, children, px, white}) => {
     const classes = `button 
     relative 
-    inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    inline-flex items-center justify-center h-10 transition-colors hover:text-color-1 ${
         px || "px-7"
     } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
     const spanClasses = "relative z-10";
