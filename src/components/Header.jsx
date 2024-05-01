22490534bb9d8cb9a1a190ddce6ac0ab0d53f12d
Furkan Demirel <furkandemireleng@gmail.com>
2024-05-01 21:37:35 +0300

    Headers response is okay

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,37 +2,68 @@ import {brainwave} from "../assets/index.js";
 import {navigation} from "../constants/index.js";
 import {useLocation} from "react-router-dom";
 import Button from "./Button.jsx";
+import MenuSvg from "../assets/svg/MenuSvg.jsx";
+import {HamburgerMenu} from "./design/Header.jsx";
+import {useState} from "react";
+import {disablePageScroll} from "scroll-lock/dist/scroll-lock.js";
+import {enablePageScroll} from "scroll-lock/dist/scroll-lock.js";
+
 
 const Header = () => {
     const pathname = useLocation();
+
+    const [openNavigation, setOpenNavigation] = useState(false);
+
+    const toggleNavigation = () => {
+        if (openNavigation) {
+            setOpenNavigation(false);
+            enablePageScroll();
+        } else {
+            setOpenNavigation(true);
+            disablePageScroll();
+        }
+    }
+
+    const handleClick = () => {
+        if (!openNavigation) return;
+        enablePageScroll();
+        setOpenNavigation(false);
+    }
+
     return (
-        <div className="fixed z-50 top-0 bg-n-8/90 backdrop-blur-sm boder-b border-n-4
-        lg:bg-n-8 lg:backdrop-blur-sm">
+        <div className={`fixed z-50 top-0 left-0 w-full bg-n-8/90 backdrop-blur-sm boder-b border-n-4
+        lg:bg-n-8 lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8' : 'bg-n-8/90 '}`}>
+
             <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
                 <a href="#hero" className="block w-[12rem] xl:mr-8">
-                    <img src={brainwave} alt="" width={190} height={40}/>
+                    <img src={brainwave} alt="brainwawe" width={190} height={40}/>
                 </a>
 
-                <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+                <nav
+                    className={` ${openNavigation ? 'flex' : 'hidden'} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
+
                     <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
                         {
                             navigation.map((item) => (
-                            <a
-                                key={item.id}
-                                href={item.url}
-                                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                                    item.onlyMobile ? "lg:hidden" : ""
-                                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                                    item.url === pathname.hash
-                                        ? "z-2 lg:text-n-1"
-                                        : "lg:text-n-1/50"
-                                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-                            >
-                                {item.title}
-                            </a>
-                        ))}
+                                <a
+                                    key={item.id}
+                                    href={item.url}
+                                    onClick={handleClick}
+                                    className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                                        item.onlyMobile ? "lg:hidden" : ""
+                                    } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                                        item.url === pathname.hash
+                                            ? "z-2 lg:text-n-1"
+                                            : "lg:text-n-1/50"
+                                    } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                                >
+                                    {item.title}
+                                </a>
+                            ))}
+
 
                     </div>
+                    <HamburgerMenu/>
                 </nav>
 
                 <a href="#new" className="button hidden mr-8 text-n-1/50 transition-colors lg:block hover:text-n-1">
@@ -41,6 +72,10 @@ const Header = () => {
 
                 <Button className="hidden lg:flex" href="#login">
                     Singin
+                </Button>
+
+                <Button className="ml-auto lg:hidden px-3" onClick={toggleNavigation}>
+                    <MenuSvg openNavigation={openNavigation}/>
                 </Button>
             </div>
         </div>
